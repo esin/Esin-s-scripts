d2840a6200fdec84542bee55b14ba48cf5d6753f
Andrey Esin <gmlastik@gmail.com>
2011-09-06 19:33:31 +0400

    [KOuntdown] - Adding README - Some cosmetic changes

--- sh/fun/kountdown/kountdown.sh
+++ sh/fun/kountdown/kountdown.sh
@@ -2,7 +2,7 @@
 DELTA_CURRENT_TIME=14400        # Delta if time on server is different from yours in secs
 CURRENT_TIME=$(expr $(date +%s) + $DELTA_CURRENT_TIME)
 COUNTDOWN_DATE="20120630 12:00:00"      # Date and time, `date` syntax
-SAVE_FOLDER="./"
+SAVE_FOLDER="."			# www-folder, where index file be stored
 DELTA_SECS=$(expr $(date -d "$COUNTDOWN_DATE" +%s) - $CURRENT_TIME)
 DELTA_DAYS=$(expr $DELTA_SECS / 86400)
 DELTA_WEEKS=$(expr $DELTA_DAYS / 7)
