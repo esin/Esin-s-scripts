8781d148c2e54fb5b1ad2d1f59052bdaa8dc5cf5
Andrey Esin <gmlastik@gmail.com>
2011-12-30 11:54:06 +0400

    [sh] - Uravn.

--- sh/uravn/server.sh
+++ sh/uravn/server.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
-
-#!/bin/bash
-AMOUNT_TRAFF=10000000000 # bytes
+AMOUNT_TRAFF=100000 # bytes
 PORT=34455
 IP=0.0.0.0 # listen connections on all interfaces
 
-FIFO="/tmp/$(base64 -w 16 /dev/urandom | head -n 1)"
+FIFO=/tmp/"$(base64 -w 16 /dev/urandom | head -n 1)"
 mkfifo $FIFO
 echo "Run wget/curl on other server for downloading"
-echo -e "HTTP/1.1 200 OK\nContent-Length: $AMOUNT_TRAFF\r\n" > $FIFO | cat /dev/urandom > $FIFO | nc -4knvl $IP $PORT < $FIFO
+echo "Port: "$PORT
+echo "I will send "$AMOUNT_TRAFF" bytes"
+echo -e "HTTP/1.1 200 OK\nContent-Length: $AMOUNT_TRAFF\r\n" > $FIFO | cat /dev/urandom > $FIFO | nc -4nvl $IP $PORT < $FIFO > /dev/null
+rm -f $FIFO
+echo "Done!"
+exit 0
+
