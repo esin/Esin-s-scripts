e3c6662d2c8feb464891aff5d6782881e56b6b1c
Andrey Esin <gmlastik@gmail.com>
2012-11-15 13:23:10 +0400

    [sh] - Yota. Style++

--- sh/yota/yota_open.sh
+++ sh/yota/yota_open.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 BLANK_PAGE=http://raw.github.com/esin/Esin-s-scripts/master/sh/yota/blank.html
-IS_302=`curl $BLANK_PAGE -s -D - | grep "HTTP/1.1 302 Found" | wc -l`
+IS_302=`curl $BLANK_PAGE --silent --compressed --dump-header - | grep "HTTP/1.1 302 Found" | wc -l`
 
 if [ $IS_302 -eq "1" ]; then
-  curl http://hello.yota.ru/php/go.php -s -d "accept_lte=1&redirurl=http://www.yota.ru/&city=msk&connection_type=sa&service_id=Service_Access_Temp"
+  curl http://hello.yota.ru/php/go.php --silent --compressed --data "accept_lte=1&redirurl=http://www.yota.ru/&city=msk&connection_type=sa&service_id=Service_Access_Temp"
 fi
