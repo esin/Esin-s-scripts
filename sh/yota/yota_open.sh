c9baf6b9f05bd30a6ccf45a8e986377eb9c1d10d
Andrey Esin <gmlastik@gmail.com>
2012-11-15 13:19:16 +0400

    [sh] - small improvement

--- sh/yota/yota_open.sh
+++ sh/yota/yota_open.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-IS_302=`curl ip.qpriv.at -s -D - | grep "HTTP/1.1 302 Found" | wc -l`
+BLANK_PAGE=http://raw.github.com/esin/Esin-s-scripts/master/sh/yota/blank.html
+IS_302=`curl $BLANK_PAGE -s -D - | grep "HTTP/1.1 302 Found" | wc -l`
 
 if [ $IS_302 -eq "1" ]; then
-  curl http://hello.yota.ru/php/go.php -d "accept_lte=1&redirurl=http://www.yota.ru/&city=msk&connection_type=sa&service_id=Service_Access_Temp"
+  curl http://hello.yota.ru/php/go.php -s -d "accept_lte=1&redirurl=http://www.yota.ru/&city=msk&connection_type=sa&service_id=Service_Access_Temp"
 fi
