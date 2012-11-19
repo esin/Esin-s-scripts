a27b4006735c7ce7571a0f9f559f6850746ec989
Andrey Esin <gmlastik@gmail.com>
2012-11-19 21:40:53 +0400

    [sh] - Yota. Retry on fail

--- sh/yota/yota_open.sh
+++ sh/yota/yota_open.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 BLANK_PAGE=http://raw.github.com/esin/Esin-s-scripts/master/sh/yota/blank.html
-IS_302=`curl $BLANK_PAGE --silent --compressed --dump-header - | grep "HTTP/1.1 302 Found" | wc -l`
+IS_302=`curl $BLANK_PAGE --retry 100000 --retry-delay 1 --silent --compressed --dump-header - | grep "HTTP/1.1 302 Found" | wc -l`
 
 if [ $IS_302 -eq "1" ]; then
   curl http://hello.yota.ru/php/go.php \
