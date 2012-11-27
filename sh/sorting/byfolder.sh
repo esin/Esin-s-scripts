7cebbca655039d84b34dfc18c24a4df63fb213ec
Andrey Esin <gmlastik@gmail.com>
2012-11-27 19:18:38 +0400

    Yaaa

--- sh/sorting/byfolder.sh
+++ sh/sorting/byfolder.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
-DATE_FORMAT=+%Y-%m-%d
-DATE_LIST=$(ls --time-style=$DATE_FORMAT /bin -tlQnoGgAr --block-size=1E | cut -f4 -d' ' | sort -u | grep -v ^$)
+# %Y - year
+# %m - number of month
+# %d - date of month
 
+
+DATE_FORMAT=+%Y-%m-%d
+DATE_LIST=$(ls --time-style=$DATE_FORMAT /bin -tlQnoGgAr --block-size=1E | grep -v ^d | cut -f4 -d' ' | sort -u | grep -v ^$)
+FILE_LIST=$(ls --time-style=$DATE_FORMAT /bin -tlQnoGgAr --block-size=1E | grep -v ^d | cut -f5 -d' ' | sort -u | grep -v ^$)
+#exit;
 #echo $DATE_LIST
-for 
+#for each in $DATE_LIST;
+#do
+ #echo $each
+#done
