dc62ad2b453b7b97d88709ba5db0e9b90f8fc6f0
Andrey Esin <gmlastik@gmail.com>
2012-11-27 23:28:58 +0400

    Works fine

--- sh/sorting/byfolder.sh
+++ sh/sorting/byfolder.sh
@@ -5,14 +5,14 @@
 
 
 DATE_FORMAT=+%Y-%m-%d
-DATE_LIST=$(ls --time-style=$DATE_FORMAT /bin -tlnoGgAr --block-size=1E | grep -v ^d | cut -f4 -d' ' | sort -u | grep -v ^$)
+DATE_LIST=$(ls --time-style=$DATE_FORMAT /bin -tlnoGgAr --block-size=1E | grep -v ^d | grep -v ^l |cut -f4 -d' ' | sort -u | grep -v ^$)
 #FILE_LIST=$(ls --time-style=$DATE_FORMAT /bin -tlQnoGgAr --block-size=1E | grep -v ^d | cut -f5 -d' ' | sort -u | grep -v ^$)
 
 for each in $DATE_LIST;
 do
  mkdir -p 1/$each;
  echo $each
- cp -av `find /bin -type f -exec ls {} --time-style=$DATE_FORMAT -tlnoGgAr --block-size=1E \; | grep $each | grep -v ^d | cut -f5 -d' ' | sort -u | grep -v ^$` 1/$each
+ cp -pv `find /bin -type f -exec ls {} --time-style=$DATE_FORMAT -tlnoGgAr --block-size=1E \; | grep $each | grep -v ^d | grep -v ^l | cut -f5 -d' ' | sort -u | grep -v ^$` 1/$each
 done
 
 #find /bin -type f -exec mkdir
