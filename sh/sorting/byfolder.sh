535439de51bea1d038cdf951f38ff41acaf93a0b
Andrey Esin <gmlastik@gmail.com>
2012-11-27 23:42:30 +0400

    style++

--- sh/sorting/byfolder.sh
+++ sh/sorting/byfolder.sh
@@ -3,28 +3,15 @@
 # %m - number of month
 # %d - date of month
 
-
+PARSE_DIR=/bin
+COPY_TO=1
 DATE_FORMAT=+%Y-%m-%d
-DATE_LIST=$(ls --time-style=$DATE_FORMAT /bin -tlnoGgAr --block-size=1E | grep -v ^d | grep -v ^l |cut -f4 -d' ' | sort -u | grep -v ^$)
-#FILE_LIST=$(ls --time-style=$DATE_FORMAT /bin -tlQnoGgAr --block-size=1E | grep -v ^d | cut -f5 -d' ' | sort -u | grep -v ^$)
+LS_PARAMS="--time-style=$DATE_FORMAT -tlnoGgAr --block-size=1E"
+DATE_LIST=$(ls $PARSE_DIR $LS_PARAMS | grep -v ^d | grep -v ^l |cut -f4 -d' ' | sort -u | grep -v ^$)
 
 for each in $DATE_LIST;
 do
- mkdir -p 1/$each;
+ mkdir -p $COPY_TO/$each;
  echo $each
- cp -pv `find /bin -type f -exec ls {} --time-style=$DATE_FORMAT -tlnoGgAr --block-size=1E \; | grep $each | grep -v ^d | grep -v ^l | cut -f5 -d' ' | sort -u | grep -v ^$` 1/$each
+ cp -pv `find $PARSE_DIR -type f -exec ls {} $LS_PARAMS \; | grep $each | grep -v ^d | grep -v ^l | cut -f5 -d' ' | sort -u | grep -v ^$` $COPY_TO/$each
 done
-
-#find /bin -type f -exec mkdir
-
-#for each in $FILE_LIST;
-#do
-# echo "/bin/$each"
-#done
-
-exit
-#echo $DATE_LIST
-#for each in $DATE_LIST;
-#do
- #echo $each
-#done
